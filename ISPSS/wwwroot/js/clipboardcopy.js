fc671ff611a5bc32731b5f1e24473a138eb7ee11
Paul Palayur <paulpalayur@gmail.com>
2024-02-09 17:16:25 +1100

    added copy text option and json file download in docker file

---
+++ ISPSS/wwwroot/js/clipboardcopy.js
@@ -0,0 +1,17 @@
+﻿const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.addEventListener('DOMContentLoaded', function () {
+    var clipboard = new ClipboardJS('.bi-clipboard');
+    clipboard.on('success', function (e) {
+
+        $('.bi-clipboard').css('display', 'none');
+        $('.bi-clipboard-check-fill').css('display', 'block');
+        window.setTimeout(toggleClipboardIcon, 1000);
+        e.clearSelection();
+    });
+});
+
+function toggleClipboardIcon() {
+    $('.bi-clipboard-check-fill').css('display', 'none');
+    $('.bi-clipboard').css('display', 'block');
+}
