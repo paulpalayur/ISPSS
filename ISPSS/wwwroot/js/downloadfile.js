862f325c5c31571e6877ba029ae5f1437c71aef4
Paul Palayur <paulpalayur@gmail.com>
2024-02-11 20:00:27 +1100

    Added download file feature and minor UI improvements

---
+++ ISPSS/wwwroot/js/downloadfile.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(".DownloadFile").click(function () {        
+        var content = $(".requirements>pre").html();
+        
+        $.ajax({
+            type: "POST",
+            url: "/Home/DownloadFile",
+            data: { content: content },
+            success: function (response) {                
+                var blob = new Blob([response], { type: 'text/plain' });
+
+                // Create a link and trigger a click to initiate download
+                var link = document.createElement("a");
+                link.href = window.URL.createObjectURL(blob);
+                link.download = "ISP_Networks.txt";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            },
+            error: function (error) {
+                console.error("Error:", error);
+            }
+        });
+    });
+});
